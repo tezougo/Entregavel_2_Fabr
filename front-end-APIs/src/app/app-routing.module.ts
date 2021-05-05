0125bcc4ed205c5c40333cd18ff17a764fd5deb6
wagner rodrigues <wagner.rodrigues@acad.ufsm.br>
2021-05-05 17:56:46 -0300

    main

--- front-end-APIs/src/app/app-routing.module.ts
+++ front-end-APIs/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Componente1Component } from './componente1/componente1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'componente1', component:Componente1Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
